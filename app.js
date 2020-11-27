a31981544640895566e89a17c929d34aac7cf720
Scorify <john.sperry1@gmail.com>
2020-11-27 15:53:21 -0800

    Start app.js

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ window.addEventListener('load', ()=> {
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
 
+            const api = ''  
+/*input api alternative for darksky weather API here:
+You can use other API's (openweathermap.org). The fundamentals are still the same, 
+just the selectors are different. For example, where Ed uses:
+
+const { temperature, summary } = data.currently   
+
+at 22.53
+
+Have to use other variables based on what the API is returning. For example:
+
+const { temp } = data.main;
+const  weather = data.weather[0].description;
+
+Then you will have to work your own way to get the icons. Font Awesome SVG icons and an If statement that ads each icon as a class to the icon span can work too.degree-section
+https://darkskyapp.github.io/skycons/ for app icons
+*/
         });
     } 
 });
